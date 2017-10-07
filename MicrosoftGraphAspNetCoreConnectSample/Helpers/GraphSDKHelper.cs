33acc165568c75f9b8d7d9b1cfedf1352539cd29
Mark Szabo <szmaorka@gmail.com>
2017-10-07 12:11:39 +0200

    Adding SampleID to the SDK Helper

--- MicrosoftGraphAspNetCoreConnectSample/Helpers/GraphSDKHelper.cs
+++ MicrosoftGraphAspNetCoreConnectSample/Helpers/GraphSDKHelper.cs
@@ -31,6 +31,9 @@ namespace MicrosoftGraphAspNetCoreConnectSample.Helpers
 
                     // Append the access token to the request
                     requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                    
+                    // This header identifies the sample in the Microsoft Graph service. If extracting this code for your project please remove.
+                    requestMessage.Headers.Add("SampleID", "aspnetcore-connect-sample");
                 }));
 
             return _graphClient;
