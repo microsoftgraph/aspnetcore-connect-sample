3e6669dc2480ba38528fa4e0093c42602692a62d
Mark Szabo <maorka@live.com>
2018-02-03 19:04:40 +0100

    Token mixup bugfix
    Turning off GraphSdkHelper reuse, as it caused to use wrong users' access tokens to get data from Microsoft Graph

--- MicrosoftGraphAspNetCoreConnectSample/Helpers/GraphSDKHelper.cs
+++ MicrosoftGraphAspNetCoreConnectSample/Helpers/GraphSDKHelper.cs
@@ -21,7 +21,6 @@ namespace MicrosoftGraphAspNetCoreConnectSample.Helpers
         // Get an authenticated Microsoft Graph Service client.
         public GraphServiceClient GetAuthenticatedClient(string userId)
         {
-            if (_graphClient != null) return _graphClient;
             _graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(
                 async requestMessage =>
                 {
